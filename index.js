ec17766afbcb6ac9065bad72ef2b07f66ee1c956
Daniel Boley <danboley1@gmail.com>
2022-09-01 12:43:38 -0400

    commit tree 6da22ca4160e64b9ab43016bca6ba2ba424f96f0

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
-// code your solution here
+//function superbowlWin(record) {
+//   if (record.result === 'W') {
+//        return record.year;
+//    }
+//    else {
+//        return undefined;
+//    }
+//}
+
+//function superbowlWin(record) {
+//    const result = record.find(element => element.result === "W")
+//    return result ? (result.year) : undefined;
+//}
+
+function superbowlWin(yearsAndResults) {
+    const result = yearsAndResults.find(record => record.result === "W");
+    return (result ? result.year : undefined);
+}
